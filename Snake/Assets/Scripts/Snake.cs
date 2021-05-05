21403628f4a2cbed41532c53eda999d1baaef2e8
Cecilia Bullarsjö <51711516+Bunslake@users.noreply.github.com>
2021-05-05 14:34:04 +0200

    Implement basic snake movement

---
+++ Snake/Assets/Scripts/Snake.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2Int gridMoveDirection;
+    private Vector2Int gridPosition;
+    private float gridMoveTimer;
+    private float gridMoveTimerMax;
+
+    private void Awake()
+    {
+        gridPosition = new Vector2Int(10, 10);
+        gridMoveTimerMax = 1f;
+        gridMoveTimer = gridMoveTimerMax;
+        gridMoveDirection = new Vector2Int(1, 0);
+    }
+    private void Update(){
+        if (Input.GetKeyDown(KeyCode.W)){
+            if (gridMoveDirection.y != -1) 
+            { 
+            gridMoveDirection.x = 0;
+            gridMoveDirection.y = +1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S)){
+            if (gridMoveDirection.y != +1)
+            {
+                gridMoveDirection.x = 0;
+                gridMoveDirection.y = -1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A)){
+            if (gridMoveDirection.x != +1)
+            {
+                gridMoveDirection.x = -1;
+                gridMoveDirection.y = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D)){
+            if (gridMoveDirection.x != -1)
+            {
+                gridMoveDirection.x = +1;
+                gridMoveDirection.y = 0;
+            }
+        }
+
+        gridMoveTimer += Time.deltaTime;
+        if (gridMoveTimer >= gridMoveTimerMax)
+        {
+            gridPosition += gridMoveDirection;
+            gridMoveTimer -= gridMoveTimerMax;
+        }
+        transform.position = new Vector3(gridPosition.x, gridPosition.y);
+    }
+}
