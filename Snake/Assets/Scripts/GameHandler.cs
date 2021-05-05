890ba681edcc7d12f93a70caecda2c447a872479
Cecilia Bullarsjö <51711516+Bunslake@users.noreply.github.com>
2021-05-05 09:58:08 +0200

    Test CMU in game handler

--- Snake/Assets/Scripts/GameHandler.cs
+++ Snake/Assets/Scripts/GameHandler.cs
@@ -1,16 +1,4 @@
-﻿/* 
-    ------------------- Code Monkey -------------------
-
-    Thank you for downloading this package
-    I hope you find it useful in your projects
-    If you have any questions let me know
-    Cheers!
-
-               unitycodemonkey.com
-    --------------------------------------------------
- */
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey;
@@ -18,12 +6,19 @@ using CodeMonkey.Utils;
 
 public class GameHandler : MonoBehaviour {
 
-    private void Start() {
+    void Start() {
         Debug.Log("GameHandler.Start");
 
-        GameObject snakeHeadGameObject = new GameObject();
-        SpriteRenderer snakeSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
-        snakeSpriteRenderer.sprite = GameAssets.i.snakeHeadSprite;
+        int number = 0;
+        FunctionPeriodic.Create(() => {
+            CMDebug.TextPopupMouse("Ding!" + number);
+            number++;
+        }, .3f);
+    }
+
+    void Update()
+    {
+        
     }
 
 }
