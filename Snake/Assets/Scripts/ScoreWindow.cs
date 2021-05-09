2508232a4d97583a38397109152632426384637d
Bunslake <51711516+Bunslake@users.noreply.github.com>
2021-05-09 22:31:25 +0200

    Set up game over highscore UI

--- Snake/Assets/Scripts/ScoreWindow.cs
+++ Snake/Assets/Scripts/ScoreWindow.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 
 public class ScoreWindow : MonoBehaviour
 {
+    private static ScoreWindow instance;
+    
     private Text scoreText;
     private void Awake()
     {
+        instance = this;
         scoreText = transform.Find("scoreText").GetComponent<Text>();
 
         Score.OnHighscorechanged += Score_OnHighscoreChanged;
@@ -28,5 +31,10 @@ public class ScoreWindow : MonoBehaviour
     {
         int highscore = Score.GetHighscore();
         transform.Find("highscoreText").GetComponent<Text>().text = "HIGHSCORE\n" + highscore.ToString();
+    }
+
+    public static void HideStatic()
+    {
+        instance.gameObject.SetActive(false);
     }
 }
